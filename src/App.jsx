feb0f3b2112ecaaa413b1789e83157d192ba01ff
Emanoel Cristhian Leão Marinho <emanuelleaomarinho@gmail.com>
2022-05-25 12:51:39 -0300

    Criando componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
+import { RepositoryList } from './components/RepositoryList';
 import './styles/global.scss';
 
 export function App() {
-    return <h1>Fala meu bom! Pegou rapazzz</h1>
+    return <RepositoryList />
 }
---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,31 @@
+export function RepositoryList() {
+    return (
+        <section className="repository-list">
+            <h1>Lista de repositórios</h1>
+
+            <ul>
+                <li>
+                    <strong>Unfomr</strong>
+                    <p>Forms in React</p>
+                    <a href=""  target="_blank">
+                        Acessar
+                    </a>
+                </li>
+                <li>
+                    <strong>Unfomr</strong>
+                    <p>Forms in React</p>
+                    <a href=""  target="_blank">
+                        Acessar
+                    </a>
+                </li>
+                <li>
+                    <strong>Unfomr</strong>
+                    <p>Forms in React</p>
+                    <a href=""  target="_blank">
+                        Acessar
+                    </a>
+                </li>
+            </ul>
+        </section>
+    );
+}
